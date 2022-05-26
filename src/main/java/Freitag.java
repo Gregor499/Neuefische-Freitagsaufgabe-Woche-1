35d7847b323be10b4486b56bc4d906cc263c5785
Gregor Dirks <gregor_renke_adrian@mailbox.tu-dresden.de>
2022-05-26 15:45:37 +0200

    Andres Feedback integriert

--- src/main/java/Freitag.java
+++ src/main/java/Freitag.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class Freitag {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] passwordList = new String[100];
+        String[] passwordList = new String[10];
         Arrays.fill(passwordList, "");
         int i=0;
 
-        System.out.println("Eingabe abbrechen mit leerer Eingabe und dann ENTER");
-        while(true) {
+        System.out.println("Maximale Passwortanzahl = " + passwordList.length + "\nEingabe abbrechen mit leerer Eingabe und dann ENTER");
+        while(i < 10) {
             System.out.println("Passwort " + (i+1) + ":");
             String line = scanner.nextLine();
             if (line == null || "".equals(line)){
@@ -23,29 +23,29 @@ public class Freitag {
 
 
         for (int i1 = 0; i1 < passwordList.length; i1++) {
-            System.out.println("Passwort " + (i1+1) + ":" + passwordList[i1] + "\nMindeslänge erfüllt: " +
-                    passwordLeastLengthCheck(passwordList[i1], 4)+
-                     "\nenthält Buchstaben und Zahlen: " + passwordContainsAtLeastOneLetterAndOneInt(passwordList[i])+
-                    "\nenthält Klein- und Großbuchstaben: " + passwordContainsLowerCaseAndUpperCaseLetters(passwordList[i]) +
-                    "\n");
+            if(passwordList[i1] != "") {
+                System.out.println("Passwort " + (i1 + 1) + ":" + passwordList[i1] + "\nMindeslänge erfüllt: " +
+                        passwordLeastLengthCheck(passwordList[i1], 4) +
+                        "\nenthält Buchstaben und Zahlen: " + passwordContainsAtLeastOneLetterAndOneInt(passwordList[i1]) +
+                        "\nenthält Klein- und Großbuchstaben: " + passwordContainsLowerCaseAndUpperCaseLetters(passwordList[i1]) +
+                        "\n");
+            }
         }
     }
 
 
-    public static boolean passwordLeastLengthCheck(String password, int least_length) {
-        return password.length() >= least_length;
+    public static boolean passwordLeastLengthCheck(String password, int leastLength) {
+        return password.length() >= leastLength;
     }
 
     public static boolean passwordContainsAtLeastOneLetterAndOneInt(String password) {
-        char[] letters = password.toCharArray();
-        boolean a = false;
         boolean atLeastOneInt = false;
         boolean atLeastOneTimeLetter = false;
 
         for (int i = 0; i < password.length(); i++) {
-            if (a = Character.isDigit(letters[i])) {
+            if (Character.isDigit(password.charAt(i))) {
                 atLeastOneInt = true;
-            } else if (a = Character.isLetter(letters[i])) {
+            } else if (Character.isLetter(password.charAt(i))) {
                 atLeastOneTimeLetter = true;
             }
         }
@@ -53,15 +53,13 @@ public class Freitag {
     }
 
     public static boolean passwordContainsLowerCaseAndUpperCaseLetters(String password) {
-        char[] letters = password.toCharArray();
-        boolean a = false;
         boolean atLeastOneLowerCase = false;
         boolean atLeastOneUpperCase = false;
 
         for (int i = 0; i < password.length(); i++) {
-            if (a = Character.isLowerCase(letters[i])) {
+            if (Character.isLowerCase(password.charAt(i))) {
                 atLeastOneLowerCase = true;
-            } else if (a = Character.isUpperCase(letters[i])) {
+            } else if (Character.isUpperCase(password.charAt(i))) {
                 atLeastOneUpperCase = true;
             }
         }
